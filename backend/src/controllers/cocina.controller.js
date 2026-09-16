bd6fd63a97b5e2788da3f68271efd15eb68697ef
anthony <jasielvilchezpuelles@gmail.com>
2026-09-15 23:01:06 -0500

    feat(cocina): mejora diseño del login, añade logout y valida estados en backend

--- backend/src/controllers/cocina.controller.js
+++ backend/src/controllers/cocina.controller.js
@@ -6,7 +6,19 @@ async function listarPedidosCocina(req, res) {
   return pedidosController.listarPedidos(req, res);
 }
 
+// MEJORA: Validación de seguridad. Evita que desde el frontend hackeen el estado
 async function cambiarEstadoCocina(req, res) {
+  const nuevoEstado = req.body.estado;
+  
+  // Array con los únicos estados que la cocina tiene permitido usar
+  const estadosPermitidosCocina = ["pendiente", "En preparación", "listo"];
+  
+  if (!estadosPermitidosCocina.includes(nuevoEstado)) {
+      return res.status(403).json({ 
+          error: `Acción denegada. El rol 'cocina' no puede cambiar el estado a '${nuevoEstado}'.` 
+      });
+  }
+
   req.body.rol = "cocina";
   return pedidosController.actualizarEstadoPedido(req, res);
 }
